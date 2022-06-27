5fcc44e0ba57f8ea87958b9310d8ad873a884631
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-27 12:08:31 -0800

    add checkout / thank you view/model

---
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Checkout
+{
+  public class CheckoutModel : PageModel
+  {
+    
+
+    public void OnGet()
+    {
+  
+    }
+  }
+}
