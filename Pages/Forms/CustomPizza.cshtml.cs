cc4e49b8e0d7b93e77ae17ac23240e6d7dd456a9
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-27 12:08:54 -0800

    add custom pizza form

---
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Forms
+{
+    public class CustomPizzaModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+        }
+    }
+}
