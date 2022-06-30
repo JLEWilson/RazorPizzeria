074eaaefd896f9e5e4800b2ff01b9b001ff14b25
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-30 14:12:22 -0800

    add toppings and price adjustments

--- Pages/Forms/CustomPizza.cshtml.cs
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,25 @@ namespace RazorPizzeria.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+
+        }
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if(Pizza.TomatoSauce) PizzaPrice += 1;
+            if(Pizza.Cheese) PizzaPrice += 1;
+            if(Pizza.Pepperoni) PizzaPrice += 1;
+            if(Pizza.Mushrooms) PizzaPrice += 1;
+            if(Pizza.Tuna) PizzaPrice += 1;
+            if(Pizza.Pineapple) PizzaPrice += 1.5f;
+            if(Pizza.Ham) PizzaPrice += 1;
+            if(Pizza.Beef) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
     }
 }
